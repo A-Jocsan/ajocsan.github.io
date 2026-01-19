126c351e55a3cea399ab22399ea255f8df96f6c1
A.Jocsan <240486@utxicotepec.edu.mx>
2026-01-18 23:19:57 -0600

    Modificacion
    Se mejoró el diseño del fondo y letra

--- JS/funciones.js
+++ JS/funciones.js
@@ -1,14 +1,12 @@
 const elements = document.querySelectorAll('.info-card, .reveal');
 
-const observer = new IntersectionObserver(
-    entries => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('visible');
-            }
-        });
-    },
-    { threshold: 0.25 }
-);
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+            observer.unobserve(entry.target);
+        }
+    });
+}, { threshold: 0.25 });
 
 elements.forEach(el => observer.observe(el));
